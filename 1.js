e41c7ecfe2744354ce9fe2c62c9bbdbf6cce4dc5
Mona Temchin <monatemchin@gmail.com>
2018-06-14 19:50:22 -0400

    Made i and j lets

--- 1.js
+++ 1.js
@@ -7,8 +7,8 @@ no repeating letters
 
 function sortByStrings(s,t) {
   var outString = "";
-  for (i = 0; i < t.length; i++)
-    for (j = 0; j < s.length; j++) {
+  for (let i = 0; i < t.length; i++)
+    for (let j = 0; j < s.length; j++) {
       if (t[i] === s[j]) {
          outString = outString + t[i];
       }
